bf28f4cfde287d55d5c985fc371ac8d54d932e03
ixone0 <jasonboy025@gmail.com>
2026-03-28 00:39:28 +0700

    fix settings

--- backend/liven/settings.py
+++ backend/liven/settings.py
@@ -71,13 +71,21 @@ WSGI_APPLICATION = 'liven.wsgi.application'
 # Database configuration via DATABASE_URL
 DATABASE_URL = os.getenv('DATABASE_URL', '')
 if DATABASE_URL:
+    import socket
+    # Resolve hostname to IPv4 to avoid IPv6 connectivity issues on EC2
+    _db_host = DATABASE_URL.split('@')[1].split(':')[0]
+    try:
+        _db_host_ipv4 = socket.getaddrinfo(_db_host, None, socket.AF_INET)[0][4][0]
+    except socket.gaierror:
+        _db_host_ipv4 = _db_host
+
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': DATABASE_URL.split('/')[-1],
             'USER': DATABASE_URL.split('://')[1].split(':')[0],
             'PASSWORD': DATABASE_URL.split(':')[2].split('@')[0],
-            'HOST': DATABASE_URL.split('@')[1].split(':')[0],
+            'HOST': _db_host_ipv4,
             'PORT': DATABASE_URL.split(':')[-1].split('/')[0],
         }
     }
